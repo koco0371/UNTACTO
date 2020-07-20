927ed28b37e339aa4f4928b7990d486cc210af40
kennycha <dudqn136@naver.com>
2020-07-20 23:38:45 +0900

    add ref code

---
+++ frontend/example code/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as MainVote } from './MainVote';
+export { default as MyVote } from './MyVote';
+export { default as CreateVote } from './CreateVote';
+export { default as AboutMe } from './AboutMe';
+export { default as ContactUs } from './ContactUs';
+export { default as Terms } from './Terms';
