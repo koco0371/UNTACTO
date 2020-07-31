a77bd0b9121b97ce776f2877d5208bf65d00ee2b
ddoong94 <helheim@naver.com>
2020-07-31 17:44:28 +0900

    Update login.js, check.js, signUp.js, logout.js

--- backend/backend_test/routes/check.js
+++ backend/backend_test/routes/check.js
@@ -1,15 +1,24 @@
 /check.js*/
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 var cookieParser = require('cookie-parser')
+var { verifyToken } = require('./tokenAuth');
 
 var mysql = require('mysql');
 
 router.use(cookieParser());
 
-router.get('/', function (req, res, next) {
+router.get('/', verifyToken, function (req, res, err) {
 	var user = req.cookies.user;
-	res.send(user);
+	var companyName = req.cookies.companyName;
+	var companyId = req.cookies.companyId;
+	res.json({
+		'user': user,
+		'companyName': companyName,
+		'companyId': companyId,
+	});
 });
 
 module.exports = router;
+
--- backend/backend_test/routes/logout.js
+++ backend/backend_test/routes/logout.js
@@ -8,9 +8,12 @@ var mysql = require('mysql');
 router.use(cookieParser());
 
 router.post('/', function (req, res, next) {
-	console.log('logout');
+	console.log('logout: ' + req.cookies.user + '.');
 	res.clearCookie('user');
+	res.clearCookie('companyName');
+	res.clearCookie('companyId');
     res.redirect('/');
 });
 
 module.exports = router;
+
