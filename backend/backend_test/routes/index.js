752e0d08532dc9ab3589bc19d4c057b8738424dc
PSW <koco0371@gmail.com>
2020-09-09 10:23:36 +0900

    Update project

--- backend/backend_test/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res)=>res.json({username:'bryan~~~'}));
-router.get('/group', (req, res)=>res.json({username:'dev group. bryan'}));
-
-module.exports = router;
--- frontend/example code/src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as MainVote } from './MainVote';
-export { default as MyVote } from './MyVote';
-export { default as CreateVote } from './CreateVote';
-export { default as AboutMe } from './AboutMe';
-export { default as ContactUs } from './ContactUs';
-export { default as Terms } from './Terms';
